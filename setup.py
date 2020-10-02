c8892949b2665ae1288fed2b980ce03855c42a15
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-10-02 17:45:17 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='fluprodia',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     description='Creating Fluid Proprety Diagrams using CoolProp',
     long_description='%s\n%s' % (
--- src/fluprodia/__init__.py
+++ src/fluprodia/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .fluid_property_diagram import FluidPropertyDiagram  # noqa: F401
