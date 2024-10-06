0a0a7e362d8b1f6344f9c407c7ab348c85c6abed
Francesco Witte <github@witte.sh>
2024-10-06 19:19:28 +0200

    Bump version

--- src/fluprodia/__init__.py
+++ src/fluprodia/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8
 """Create beautiful fluid property diagrams using CoolProp and matplotlib"""
 
-__version__ = '3.3'
+__version__ = '3.4'
 
 from .fluid_property_diagram import FluidPropertyDiagram  # noqa: F401
