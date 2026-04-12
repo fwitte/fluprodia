3166c5734fb20bd9fbc6ad6f1eec6e7fe0a06f98
Francesco Witte <github@witte.sh>
2026-04-12 10:32:22 +0200

    Throw future warning for deprecated python

--- src/fluprodia/__init__.py
+++ src/fluprodia/__init__.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8
 """Create beautiful fluid property diagrams using CoolProp"""
 
+import sys
+import warnings
+
 __version__ = '4.1'
+
+if sys.version_info < (3, 11):
+    warnings.warn(
+        f"Python {sys.version_info.major}.{sys.version_info.minor} is no "
+        f"longer supported as of the next major release of fluprodia. "
+        f"Please upgrade to Python 3.11 or newer.",
+        FutureWarning
+    )
 
 from .fluid_property_diagram import FluidPropertyDiagram  # noqa: F401
